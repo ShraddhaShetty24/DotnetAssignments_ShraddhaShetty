d81ecc410543b3a20f37387623115878dfe31c18
SHRADDHA SHETTY <62455729+ShraddhaShetty24@users.noreply.github.com>
2023-02-19 21:48:58 +0530

    Add files via upload

---
+++ AbstractExample_feb2/AbstractExample_feb2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample_feb2
+{
+    //abstract class Vehicles
+    //{
+    //     public abstract void running();
+        
+    //}
+    //class Car: Vehicles
+    //{
+    //    public override void running()
+    //     {
+    //        Console.WriteLine("Hello ");
+    //    }
+
+    //}
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Car object = new Car();
+    //        object.running();
+    //    }
+    //}
+
+    // Abstract class
+abstract class Animal
+{
+  
+  public abstract void animalSound();
+  // Regular method
+  public void sleep()
+  {
+    Console.WriteLine("SLEEP");
+  }
+}
+
+// Derived class (inherit from Animal)
+class Dog : Animal
+{
+  public override void animalSound()
+  {
+    Console.WriteLine("The dog says: bow bow");
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Dog myDog = new Dog(); // Create a Pig object
+    myDog.animalSound();  // Call the abstract method
+    myDog.sleep();  // Call the regular method
+  }
+}
+}
